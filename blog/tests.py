a3b231799cf1000344f5039408ab7bd978a009b4
nikhil <nikhil@nik.home>
2021-06-26 23:31:17 +0100

    Functionality added: Create a post, Edit a post, Delete a post

--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,24 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Test Title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+        'title' : 'New Title',
+        'body' : 'New Text',
+        'author': self.user.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New Title')
+        self.assertEqual(Post.objects.last().body, 'New Text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'),{
+        'title' : 'Update title',
+        'body' : 'Updated text'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
